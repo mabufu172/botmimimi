97413c871ddddbc9d6ff5fc3e25c8e9e9705cd8c
mabufu172 <reidev241@gmail.com>
2025-11-03 21:09:43 +0700

    no longer using promisify as glob from glob already returns a promise

--- src/handler/index.js
+++ src/handler/index.js
@@ -1,21 +1,19 @@
 const { glob } = require('glob')
-const { promisify } = require('util')
-const globPromise = promisify(glob)
 
 module.exports = async (client) => {
 
-    const commandFiles = await globPromise(`${process.cwd()}/src/commands/**/*.js`)
+    const commandFiles = await glob('src/commands/**/*.js', { absolute: true })
     commandFiles.map((value) => {
         const file = require(value)
-        const splitted = value.split('/')
+        const splitted = value.split(/[\\/]/)
         const directory = splitted[splitted.length - 2]
 
         if (file.name) {
             const properties = { directory, ...file }
             client.commands.set(file.name, properties)
         }
-    })
+    });
 
-    const eventFiles = await globPromise(`${process.cwd()}/src/events/*.js`)
+    const eventFiles = await glob('src/events/*.js', { absolute: true })
     eventFiles.map((value) => require(value))
-}
+};
