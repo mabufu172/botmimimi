bdcf32556a8989160426d34dacd46dfca89803cf
mabufu172 <reidev241@gmail.com>
2025-11-04 20:47:51 +0700

    removed unused parameter and directly using numbers for the msInX

--- src/utils/msToDHMS.js
+++ src/utils/msToDHMS.js
@@ -1,9 +1,9 @@
-module.exports = (ms, showUnused) => {
+module.exports = ms => {
 
     const msInASecond = 1000
-    const msInAMinute = 60 * msInASecond
-    const msInAnHour = 60 * msInAMinute
-    const msInADay = 24 * msInAnHour
+    const msInAMinute = 60000
+    const msInAnHour = 3600000
+    const msInADay = 86400000
 
     let
     days = 0,
@@ -17,21 +17,15 @@ module.exports = (ms, showUnused) => {
         days = (remaining_ms - (remaining_ms % msInADay)) / msInADay
         remaining_ms -= days * msInADay
         clock += days + "d"
-    }
-
-    if (ms >= msInAnHour) {
+    } else if (ms >= msInAnHour) {
         hours = (remaining_ms - (remaining_ms % msInAnHour)) / msInAnHour
         remaining_ms -= hours * msInAnHour
         clock += hours + "h"
-    }
-
-    if (ms >= msInAMinute) {
+    } else if (ms >= msInAMinute) {
         minutes =  (remaining_ms - (remaining_ms % msInAMinute)) / msInAMinute
         remaining_ms -= minutes * msInAMinute
         clock += minutes + "m"
-    }
-
-    if (ms >= msInASecond) {
+    } else if (ms >= msInASecond) {
         seconds = (remaining_ms - (remaining_ms % msInASecond)) / msInASecond
         remaining_ms -= minutes * msInASecond
         clock += seconds + "s"
