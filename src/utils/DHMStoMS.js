10e854bc75353e1fa74d376079577eee2ef604af
mabufu172 <reidev241@gmail.com>
2025-11-04 20:49:52 +0700

    created DHMStoMS function

---
+++ src/utils/DHMStoMS.js
@@ -0,0 +1,28 @@
+module.exports = DHMS => {
+
+    const signs = DHMS.match(/[dhms]/gi)
+    const numbers = DHMS.split(/[dhms]/gi)
+
+    let result = 0
+
+    signs.forEach((char, index) => {
+        let multiplier = 0
+        switch(char){
+            case "s":
+                multiplier = 1000
+                break
+            case "m":
+                multiplier = 60000
+                break
+            case "h":
+                multiplier = 3600000
+                break
+            case "d":
+                multiplier = 86400000
+                break
+        }
+        result += numbers[index] * multiplier
+    })
+
+    return result
+}
