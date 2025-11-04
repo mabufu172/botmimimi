ad63d06dfde950ea2a1e126f03851db5fd762906
mabufu172 <reidev241@gmail.com>
2025-11-04 19:59:35 +0700

    getUniqueID now require parameters

--- src/utils/getUniqueID.js
+++ src/utils/getUniqueID.js
@@ -1,1 +1,6 @@
-module.exports = () => Math.floor(Math.random() * 999) + new Date().getTime().toString().slice(-5)
+// randomLimit default to 999
+// getLastDateDigits default to -5
+
+module.exports = (randomLimit, getLastDateDigits) => {
+    return Math.floor(Math.random() * randomLimit) + new Date().getTime().toString().slice(getLastDateDigits)
+}
