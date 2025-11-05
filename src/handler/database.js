4a0a8bfe72b02972c06ae7d67d93246893dcf6a7
mabufu172 <reidev241@gmail.com>
2025-11-05 09:14:10 +0700

    changed reminder to reminders

--- src/handler/database.js
+++ src/handler/database.js
@@ -3,7 +3,7 @@ const bs3 = require('better-sqlite3')
 const Database = bs3('data.db', { verbose: console.log })
 Database.exec(
     `
-    CREATE TABLE IF NOT EXISTS reminder (
+    CREATE TABLE IF NOT EXISTS reminders (
     userId TEXT,
     reminderId TEXT,
     expiryDate TEXT,
